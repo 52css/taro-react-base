986f4f540510987c7e6a323765e4284bb3c7a84d
52css <jie_wei@126.com>
2023-02-02 14:50:26 +0800

    fab 支持className\style\click

--- src/components/fab/index.tsx
+++ src/components/fab/index.tsx
@@ -1,12 +1,27 @@
 import { View, Text } from '@tarojs/components'
+import classNames from 'classnames'
 import { Button } from '@/components/index'
 import { IFabProps } from './type'
 import './index.scss'
 
-export default function ({ buttonProps, icon, text, onClick }: IFabProps) {
+export default function ({ buttonProps, icon, text, className, style, onClick }: IFabProps) {
+  const rootClassName = classNames('fab', className)
+
+  const getStyle = () => {
+    const mergedStyle: React.CSSProperties = { ...style };
+
+    return mergedStyle
+  }
+
+  const handleClick = (e) => {
+    onClick && onClick(e)
+  }
+
   return (
-    <View className='fab'>
-      <Button {...buttonProps} size='large' shape={text ? 'round' : 'circle'} theme='primary' children={text} icon={icon} onClick={onClick}></Button>
+    <View className={rootClassName} style={getStyle()} onClick={handleClick}>
+      <Button {...buttonProps} size='large' shape={text ? 'round' : 'circle'} theme='primary' icon={icon} onClick={onClick}>
+        {text}
+      </Button>
     </View>
   );
 }
--- src/components/fab/type.ts
+++ src/components/fab/type.ts
@@ -1,10 +1,12 @@
 import { MouseEvent } from 'react';
-import { TNode, TElement, SizeEnum } from '../common';
-import { IButtonProps } from '@/components/button/type'
+import { IButtonProps } from '@/components/button/type';
+import { TNode, TElement, SizeEnum, Styles } from '../common';
 
 export interface IFabProps {
   buttonProps?: IButtonProps;
   icon?: TElement;
   text?: string;
+  className?: string;
+  style?: Styles;
   onClick?: (e: MouseEvent<HTMLElement>) => void;
 }
